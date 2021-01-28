a9e29242b3d6bc68c92ec10bd3a5c27b3d8ac429
Jay-prog321 <469837460@qq.com>
2021-01-28 11:41:09 +0800

    test update

---
+++ action/actionGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class actionGame : MonoBehaviour
+{
+    [SerializeField]
+    actionRole role=default;
+
+    Vector2 Pos = new Vector2(0, 1.73f);
+    private void Awake()
+    {
+        role.Init(Pos);
+    }
+}
---
+++ action/actionUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using FairyGUI;
+using UnityEngine.UI;
+
+public class actionUI : MonoBehaviour
+{
+    public JoystickModule _joystick;
+    [SerializeField]
+    Text X,Y;
+    public Transform _transform;
+    void Start()
+    {
+        GComponent mainView = GetComponent<UIPanel>().ui;
+        _joystick = new JoystickModule(mainView);
+    }
+    private void Update()
+    {
+        //_joystick.Control(_transform,0.0005f);
+        //X.text = string.Format("X:{0}", _transform.position.x);
+        //Y.text = string.Format("Y:{0}", _transform.position.z);
+    }
+}
