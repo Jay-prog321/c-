8cf8cca30cbc59e304bc017191f85243970410ff
Jay-prog321 <469837460@qq.com>
2021-02-01 14:10:06 +0800

    lua update

---
+++ LearnEnglish - lua/LearnEnglish/Word.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnEnglish
+{   
+    public class Word
+    {
+        public string Chinese;
+        public string English;
+        public Word(string chinese,string english)
+        {
+            Chinese = chinese;
+            English = english;
+        }
+    }
+}
